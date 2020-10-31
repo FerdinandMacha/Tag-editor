8372a13912f8c672f39de77d100fce7e0aaaebd7
F.Macha <f.macha@seznam.cz>
2020-10-31 13:23:17 +0100

    finishing

--- tags_storage.py
+++ tags_storage.py
@@ -3,7 +3,6 @@ import logging
 
 # contains the instances of tags_model.TagCategory
 tag_configuration = list()
-flat_tags = dict()
 
 
 def load_tag_configuration(config_file_name):
@@ -26,14 +25,8 @@ def load_tag_configuration(config_file_name):
                 is_heading = False
             else :
                 tag_configuration[current_heading_index].add_item(tag_line)
-                tag_key = tag_line.upper()
-                if not tag_key in flat_tags:
-                    flat_tags[tag_key] = tag_configuration[current_heading_index]
 
     log_tags('Loaded configuration:', tag_configuration)
-    logging.debug('Loaded tag keys:')
-    for k in flat_tags:
-        logging.debug(k)
 
 
 def load_tags(tags_file_name):
@@ -43,25 +36,30 @@ def load_tags(tags_file_name):
             next(f)
             
             # strip '<div>' from left and '</div>\n' from right for the tag name
-            result = {line[5:-7]: True for line in f}
+            result = {get_tag_key(line[5:-7]): True for line in f}
         return result
 
+    def get_tag_key(tag_name):
+        return tag_name.upper()
 
     current_tags = load_current_tags()
-    for t in current_tags:
-        logging.debug(f'Tag from file: {t}')
+    [ logging.debug(f'Loaded from file: {t}') for t in current_tags ]
+
 
     result = list()
     for tag_category in tag_configuration:
         category_tags = tags_model.TagCategory((tag_category.name, None))
         result.append(category_tags)
         for tag in tag_category.items:
-            current_tag = category_tags.add_item((tag.name, category_tags))
-            current_tag.included = True if current_tags.pop(tag.name, False) else False
+            current_tag = category_tags.add_item(tag.name)
+            current_tag.included = True if current_tags.pop(get_tag_key(tag.name), False) else False
 
-    for tag_name in current_tags:
-        
-        if flat_tags.get(tag_name.upper(), None):
+    if len(current_tags):
+        additional = tags_model.TagCategory(('Additional tags', None))
+        for tag_name in current_tags:
+            current_tag = additional.add_item(tag_name)
+            current_tag.included = True
+
 
     log_tags('Loaded file tags:', result)
     return result
@@ -78,8 +76,7 @@ def save_tags(tags_file_name, tag_categories):
 def log_tags(list_type, tag_list):
     logging.debug(list_type)
     for category in tag_list:
-        for tag in category.items:
-            logging.debug(f'{category.name} : {tag.__dict__}')
+        [ logging.debug(f'{category.name} : {tag.__dict__}') for tag in category.items ]
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
