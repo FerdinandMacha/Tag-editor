14d774c7ba7e453307d5b38a6e24ebf2479df597
Ferdinand Macha <fmacha@localhost.localdomain>
2020-11-22 08:30:04 +0100

    typing storage WIP

--- tags_storage.py
+++ tags_storage.py
@@ -1,20 +1,19 @@
-from typing import cast
-from tags_model import TagCategoryBaseItem, TagItem, TagCategoryBase, TagCategory
+from typing import Dict, Union, cast
 import logging
 
-from tags_model import TagCategoryBaseItem
-
-# contains the instances of tags_model.TagCategory
-tag_configuration = list()
+from tags_model import TagCategoryBaseItem, TagItem, TagCategoryBase, TagCategory
 
 
-def load_tag_configuration(config_file_name):
+tag_configuration: list[TagCategoryBase] = list()
+
+
+def load_tag_configuration(config_file_name: str)-> None:
     with open(config_file_name, mode='r', encoding='utf-8-sig') as f:
-        is_heading=True
-        current_heading_index = -1
+        is_heading: bool = True
+        current_heading_index: int = -1
 
         for line in f:
-            tag_line = line.strip()
+            tag_line: str = line.strip()
             if not tag_line:
                 is_heading = True
 
@@ -32,8 +31,8 @@ def load_tag_configuration(config_file_name):
     log_tags('Loaded configuration:', tag_configuration)
 
 
-def load_tags(tags_file_name):
-    def load_current_tags():
+def load_tags(tags_file_name: str)-> list[TagCategory]:
+    def load_current_tags()-> Dict[str, bool]:
         with open(tags_file_name, mode='r', encoding='utf-8-sig') as f:
             # Skip <!DOCTYPE html> header line
             next(f)
@@ -42,42 +41,50 @@ def load_tags(tags_file_name):
             result = {get_tag_key(line[5:-7]): True for line in f}
         return result
 
-    def get_tag_key(tag_name):
+    def get_tag_key(tag_name: str)-> str:
         return tag_name.upper()
 
-    current_tags = load_current_tags()
-    [ logging.debug(f'Loaded from file: {t}') for t in current_tags ]
+    def load_current_tag(tag_config: TagCategoryBase, loaded_tags: list[TagCategory], 
+        included_predicate: callable[[TagItem], bool])-> TagCategory:
+        result: TagCategory = TagCategory((tag_config.name, None))
+        loaded_tags.append(result)
+        for tag in tag_config.items:
+            current_tag: TagItem = cast(TagItem, result.add_item(tag.name))
+            current_tag.included = included_predicate(tag)
 
-
-    result = list()
+    current_tags: Dict[str, bool] = load_current_tags()
+    result: list[TagCategory] = list()
     for tag_category in tag_configuration:
-        category_tags: TagCategory = TagCategory((tag_category.name, None))
+        load_current_tag(tag_category, current_tags, 
+            lambda t: True if current_tags.pop(get_tag_key(t.name), False) else False)
+
+        """ category_tags: TagCategory = TagCategory((tag_category.name, None))
         result.append(category_tags)
         for tag in tag_category.items:
-            current_tag: TagCategoryBaseItem = category_tags.add_item(tag.name)
-            cast(TagItem, current_tag).included = True if current_tags.pop(get_tag_key(tag.name), False) else False
+            current_tag: TagItem = cast(TagItem, category_tags.add_item(tag.name))
+            current_tag.included = True if current_tags.pop(get_tag_key(tag.name), False) else False """
 
     if len(current_tags):
         additional: TagCategory = TagCategory(('Additional tags', None))
         for tag_name in current_tags:
-            current_tag: TagCategoryBaseItem = additional.add_item(tag_name)
-            cast(TagItem, current_tag).included = True
+            current_tag: TagItem = cast(TagItem, additional.add_item(tag_name))
+            current_tag.included = True
 
 
     log_tags('Loaded file tags:', result)
     return result
                 
 
-def save_tags(tags_file_name, tag_categories):
+def save_tags(tags_file_name: str, tag_categories: list[str])-> None:
     with open(tags_file_name, mode='w', encoding='utf-8-sig') as f:
         f.write('<!DOCTYPE html>\n')
         for tag in tag_categories:
-            _ = f.write('<div>'+tag+'</div>\n')        
+            _ = f.write(f'<div>{tag}</div>\n')
 
 
 
-def log_tags(list_type, tag_list):
-    logging.debug(list_type)
+def log_tags(list_description: str, tag_list: Union[list[TagCategoryBase], list[TagCategory]])-> None:
+    logging.debug(list_description)
     for category in tag_list:
         [ logging.debug(f'{category.name} : {tag.__dict__}') for tag in category.items ]
 
