115efbf255defdb057de0eb537f44843d11be332
Ferdinand Macha <F.Macha@seznam.cz>
2021-02-07 19:13:02 +0100

    WIP: cmd args

---
+++ main.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import os.path
+import sys
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gio, Gtk
+
+import tags_storage
+from tags_view import TagsView
+
+
+class TagFileFilter(Gtk.FileFilter):
+    def __init__(self):
+        Gtk.FileFilter.__init__(self)
+        self.set_name("Tag files")
+        self.add_pattern("*-tags.html")
+
+
+
+class TagsApp(Gtk.Application):
+
+    C_PROVIDE_CONFIGURATION_MESSAGE = "You have to provide the tags configuration."
+
+    def __init__(self):
+        super().__init__(application_id='fmacha.gtk.tags-editor',
+                         flags=Gio.ApplicationFlags.HANDLES_OPEN)
+
+    def do_open(self, files, *hint):
+        import logging
+        logging.debug(files[0])
+        self.do_activate()
+        return 0
+
+    def do_activate(self):
+        if len(sys.argv) == 2:
+            # just a basic first check
+            if os.path.isfile(sys.argv[1]):
+                self.show_tags_window(sys.argv[1])
+        else:
+            self.show_tags_window(self.choose_all_tags())
+
+
+    def show_tags_window(self, all_tags_file_name):
+            try:
+                tags_storage.load_tag_configuration(all_tags_file_name)
+                win = TagsView(application=self)
+                win.show_all()
+            except:
+                dialog = Gtk.MessageDialog(
+                    flags=0,
+                    message_type=Gtk.MessageType.ERROR,
+                    buttons=Gtk.ButtonsType.CANCEL,
+                    text="The tags configuration is invalid.",
+                )
+                dialog.format_secondary_text(
+                    "The file\n" + all_tags_file_name +
+                    "\ncontent cannot be loaded as tags.\nPerhaps it is not a configuration file at all?"
+                )
+                dialog.run()
+                dialog.destroy()
+
+
+    def choose_all_tags(self):
+        builder: Gtk.Builder = Gtk.Builder()
+        builder.add_from_file("dlg_configuration.ui")
+
+        dialog: Gtk.FileChooserDialog = builder.get_object("dlg_config_chooser")
+        '''dialog = Gtk.FileChooserDialog(
+            title="Please choose a tags configuration file",
+            parent=None,
+            action=Gtk.FileChooserAction.OPEN
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL,
+            Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_OPEN,
+            Gtk.ResponseType.OK,
+        )
+        dialog.add_filter(TagFileFilter())'''
+
+        response = dialog.run()
+        result = dialog.get_filename()
+        dialog.destroy()
+
+        if response == Gtk.ResponseType.CANCEL:
+            raise ValueError (TagsApp.C_PROVIDE_CONFIGURATION_MESSAGE)
+        return result
+
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+app = TagsApp()
+exit_status = app.run(sys.argv)
+sys.exit(exit_status)
--- tags_view.py
+++ tags_view.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gio, Gtk
-
-import os.path
-import sys
+from gi.repository import Gtk
 
 import tags_storage
-from tags_model import (TagCategory, TagCategoryBase, TagCategoryBaseItem, TagItem)
+from tags_model import (TagCategory, TagItem)
 
 import logging
 
@@ -70,85 +67,11 @@ class TagsView(Gtk.ApplicationWindow):
         self.save_to_file()
 
     @Gtk.Template.Callback()
+    def on_bt_open_file_set(self, button):
+        self.reload_window(button.get_filename(), self.tags_view, self.top_bar)
+
+    @Gtk.Template.Callback()
     def name_changed(self, widget, path, text):
         self.tree_store[self.get_tree_store_path(path)][0] = text
         self.save_to_file()
-
-    @Gtk.Template.Callback()
-    def on_bt_open_file_set(self, button):
-        self.reload_window(button.get_filename(), self.tags_view, self.top_bar)
         
-
-class TagFileFilter(Gtk.FileFilter):
-    def __init__(self):
-        Gtk.FileFilter.__init__(self)
-        self.set_name("Tag files")
-        self.add_pattern("*-tags.html")
-
-
-class TagsApp(Gtk.Application):
-
-    C_PROVIDE_CONFIGURATION_MESSAGE = "You have to provide the tags configuration."
-
-    def __init__(self):
-        super().__init__(application_id='fmacha.gtk.tags-editor',
-                         flags=Gio.ApplicationFlags.HANDLES_OPEN)
-
-    def do_activate(self):
-        if len(sys.argv) == 2:
-            # just a basic first check
-            if os.path.isfile(sys.argv[1]):
-                self.show_tags_window(sys.argv[1])
-        else:
-            self.show_tags_window(self.choose_all_tags())
-
-
-    def show_tags_window(self, all_tags_file_name):
-            try:
-                tags_storage.load_tag_configuration(all_tags_file_name)
-                win = TagsView(application=self)
-                win.show_all()
-            except:
-                dialog = Gtk.MessageDialog(
-                    flags=0,
-                    message_type=Gtk.MessageType.ERROR,
-                    buttons=Gtk.ButtonsType.CANCEL,
-                    text="The tags configuration is invalid.",
-                )
-                dialog.format_secondary_text(
-                    "The file\n" + all_tags_file_name +
-                    "\ncontent cannot be loaded as tags.\nPerhaps it is not a configuration file at all?"
-                )
-                dialog.run()
-                dialog.destroy()
-
-
-    def choose_all_tags(self):
-        dialog = Gtk.FileChooserDialog(
-            title="Please choose a tags configuration file",
-            parent=None,
-            action=Gtk.FileChooserAction.OPEN
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL,
-            Gtk.ResponseType.CANCEL,
-            Gtk.STOCK_OPEN,
-            Gtk.ResponseType.OK,
-        )
-        dialog.add_filter(TagFileFilter())
-
-        response = dialog.run()
-        result = dialog.get_filename()
-        dialog.destroy()
-
-        if response == Gtk.ResponseType.CANCEL:
-            raise ValueError (TagsApp.C_PROVIDE_CONFIGURATION_MESSAGE)
-        return result
-
-
-    def do_startup(self):
-        Gtk.Application.do_startup(self)
-
-app = TagsApp()
-exit_status = app.run(sys.argv)
-sys.exit(exit_status)
